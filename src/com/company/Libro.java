b606233b56ac6c327825262624a1f1e4e0a58b46
Juani Unzurrunzaga <juanunzu@gmail.com>
2020-03-20 10:27:07 -0300

    Parte E - agrego atrib lista ejemplares en clase libro + metodo agregar

--- src/com/company/Libro.java
+++ src/com/company/Libro.java
@@ -1,15 +1,28 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libro {
 
   private String nombre;
   private Integer codigoIsbn;
   private String autor;
+  private List<Ejemplar> listaDeEjemplares;
 
   public Libro(String nombre, Integer codigoIsbn, String autor) {
     this.nombre = nombre;
     this.codigoIsbn = codigoIsbn;
     this.autor = autor;
+    this.listaDeEjemplares = new ArrayList<Ejemplar>();
+  }
+
+  public void agregarEjemplar(Ejemplar unEjemplar){
+    this.listaDeEjemplares.add(unEjemplar);
+  }
+
+  public void quitarEjemplar(Ejemplar unEjemplar){
+    this.listaDeEjemplares.remove(unEjemplar);
   }
 
   public String getNombre() {
