d62cffd62df2936889f14c10554aa2ddf358e882
Juani Unzurrunzaga <juanunzu@gmail.com>
2020-03-20 10:38:34 -0300

    Parte F - se modifican clases socio y sociovip

--- src/com/company/Socio.java
+++ src/com/company/Socio.java
@@ -1,15 +1,22 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Socio {
 
   private String nombre;
   private String apellido;
   private Integer numeroIdentificacion;
+  private List<Ejemplar> ejemplaresRetirados;
+  public static final Integer LIBROS_AUTORIZADOS_A_RETIRAR = 3;
+
 
   public Socio(String nombre, String apellido, Integer numeroIdentificacion) {
     this.nombre = nombre;
     this.apellido = apellido;
     this.numeroIdentificacion = numeroIdentificacion;
+    this.ejemplaresRetirados = new ArrayList<Ejemplar>();
   }
 
   public String getNombre() {
--- src/com/company/SocioVip.java
+++ src/com/company/SocioVip.java
@@ -3,6 +3,7 @@ package com.company;
 public class SocioVip extends Socio {
 
   private Integer valorCuotaMensual;
+  public static final Integer LIBROS_AUTORIZADOS_A_RETIRAR = 15;
 
   public SocioVip(String nombre, String apellido, Integer numeroIdentificacion, Integer valorCuotaMensual) {
     super(nombre, apellido, numeroIdentificacion);
