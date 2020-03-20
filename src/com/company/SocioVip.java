58db5da88c14d621b5feecbd7d0c1a61d08ccf90
Juani Unzurrunzaga <juanunzu@gmail.com>
2020-03-20 10:11:41 -0300

    Parte C - creo clase socio vip, subclase de socio + constr + getter

---
+++ src/com/company/SocioVip.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class SocioVip extends Socio {
+
+  private Integer valorCuotaMensual;
+
+  public SocioVip(String nombre, String apellido, Integer numeroIdentificacion, Integer valorCuotaMensual) {
+    super(nombre, apellido, numeroIdentificacion);
+    this.valorCuotaMensual = valorCuotaMensual;
+  }
+
+  public Integer getValorCuotaMensual() {
+    return valorCuotaMensual;
+  }
+}
